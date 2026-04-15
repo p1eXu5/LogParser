e576e65f34a7aacbe5d6ada772cc36f9ba041921
p1eXu5 <v1adp1exu5@gmail.com>
2026-04-15 19:15:08 +0300

    fix: Helpers.decodeUnicodeEscapes

--- src/LogParser.ElmishApp/Helpers.fs
+++ src/LogParser.ElmishApp/Helpers.fs
@@ -8,7 +8,8 @@ let notEmpty s = not (String.IsNullOrWhiteSpace(s))
 
 let decodeUnicodeEscapes input =
     // Pattern matches literal \u followed by 4 hex digits
-    let pattern = @"\\u([a-fA-F0-9]{4})"
+    let pattern1 = @"\\\\u([a-fA-F0-9]{4})"
+    let pattern2 = @"\\u([a-fA-F0-9]{4})"
     
     let evaluator = MatchEvaluator(fun m ->
         // Extract the 4-digit hex code from the first capturing group
@@ -19,4 +20,5 @@ let decodeUnicodeEscapes input =
         char charCode |> string
     )
 
-    Regex.Replace(input, pattern, evaluator)
+    Regex.Replace(input, pattern1, evaluator)
+    |> fun res -> Regex.Replace(res, pattern2, evaluator)
