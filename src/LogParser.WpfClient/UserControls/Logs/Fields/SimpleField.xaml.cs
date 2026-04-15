f00713f0038ef5c0c8a72d67cf4009234399b3e6
p1eXu5 <v1adp1exu5@gmail.com>
2026-04-15 19:29:25 +0300

    git: add ignored files

---
+++ src/LogParser.WpfClient/UserControls/Logs/Fields/SimpleField.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogParser.WpfClient.UserControls.Logs.Fields;
+/// <summary>
+/// Interaction logic for SimpleField.xaml
+/// </summary>
+public partial class SimpleField : UserControl
+{
+    public SimpleField()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/LogParser.WpfClient/UserControls/Logs/TechLog.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogParser.WpfClient.UserControls.Logs;
+/// <summary>
+/// Interaction logic for TechLog.xaml
+/// </summary>
+public partial class TechLog : UserControl
+{
+    public TechLog()
+    {
+        InitializeComponent();
+    }
+}
