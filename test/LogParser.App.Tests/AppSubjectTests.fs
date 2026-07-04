72f91820194e6482057abd2e3198aace88c45101
p1eXu5 <v1adp1exu5@gmail.com>
2026-07-04 08:27:46 +0300

    test: adjust AppSubject tests

--- test/LogParser.App.Tests/AppSubjectTests.fs
+++ test/LogParser.App.Tests/AppSubjectTests.fs
@@ -1,44 +1,67 @@
 namespace LogParser.App.Tests
 
 open System
-open Microsoft.Extensions.Logging
+open System.Threading
 
 open NUnit.Framework
 open Faqt
 open Faqt.Operators
-open FSharp.Control.Reactive.Testing
-open LogParser.Tests.Fakers
-open p1eXu5.AspNetCore.Testing.Logging
-
-open FSharp.Control.Reactive
+open p1eXu5.FSharp.Reactive
+open p1eXu5.FSharp.Reactive.Testing
 
 open LogParser.App
+open LogParser.Types
+open LogParser.Tests.Fakers
 
 module AppSubjectTests =
+    let private appSubject (parserSubscriptionBatchSize: int) =
+        let appConfig =
+            {
+                ParserSubscriptionBatchSize = parserSubscriptionBatchSize
+                ParserBatchFlushTimeSpan = TimeSpan.FromMilliseconds(200.0)
+            }
+        AppSubject.init AppSubjectLogger.Console appConfig
 
-    let private collect (appSubject: AppSubject) =
-        let buf = ResizeArray<Guid * LogParseMsg>()
+    let private createBufAndSubscribe (appSubject: AppSubject) =
+        let buf = ResizeArray<LogSourceId * LogPosition list>()
         let sub =
-            (appSubject :> IObservable<Guid * LogParseMsg>)
-                .Subscribe(fun item -> buf.Add(item))
+            appSubject
+            :> IObservable<LogSourceId * LogPosition seq>
+            |> Observable.subscribeNext
+                (fun (id, positions) ->
+                    let now = DateTimeOffset.Now.ToString("HH':'mm':'ss.fffff")
+                    printfn "[%s] LogParser.App.Tests.AppSubjectTests\n\tLogPosition seq for %A received (Thread #%i)" now id Thread.CurrentThread.ManagedThreadId
+                    buf.Add((id, Seq.toList positions))
+                )
         buf, sub
 
-    let private appSubject () = new AppSubject(TestLogger<AppSubject>(TestContextWriters.GetInstance()) :> ILogger<AppSubject>)
+    let private msg () = LogPosition.generate ()
+    let private errMsg () = Guid.NewGuid().ToString("N") |> LogParseMsg.ParsingError
 
-    let msg () = LogPosition.generate () |> LogParseMsg.LogPosition
-    let errMsg () = Guid.NewGuid().ToString("N") |> LogParseMsg.ParsingError
+    let spinWait buffCount (buf: ResizeArray<LogSourceId * LogPosition list>) =
+        let res = SpinWait.SpinUntil(Func<bool> (fun _ -> buf.Count = buffCount), 5000)
+        %res.Should().BeTrue()
+
+    let spinWaitItems (lastItemsCounts: int list) (buf: ResizeArray<LogSourceId * LogPosition list>) =
+        let res = SpinWait.SpinUntil(Func<bool> (fun _ -> buf |> Seq.map (snd >> _.Length) |> Seq.toList |> List.sort |> (=) (lastItemsCounts |> List.sort)), 5000)
+        %res.Should().BeTrue()
+
+    [<SetUp>]
+    let logTestThread () =
+        let now = DateTimeOffset.Now.ToString("HH':'mm':'ss.fffff")
+        printfn "[%s] LogParser.App.Tests.AppSubjectTests\n\tStarting test (Thread #%i)" now Thread.CurrentThread.ManagedThreadId
 
     [<Test>]
-    let ``GetObserver returns non-null observer`` () =
-        let appSubject = appSubject ()
-        let obs = appSubject.GetObserver(Guid.NewGuid())
+    let ``01: GetObserver returns non-null observer`` () =
+        let appSubject = appSubject 1
+        let obs = appSubject.GetObserver(LogSourceId.create ())
         %obs.Should().NotBeNull()
 
     [<Test>]
-    let ``Messages from a single observer reach subscribers`` () =
-        let appSubject = appSubject ()
-        let buf, _ = collect appSubject
-        let id = Guid.CreateVersion7()
+    let ``02: Messages from a single observer reach subscribers`` () =
+        let appSubject = appSubject 2
+        let buf, _ = createBufAndSubscribe appSubject
+        let id = LogSourceId.create ()
         let msg0 = msg ()
         let msg1 = msg ()
 
@@ -46,16 +69,17 @@ module AppSubjectTests =
         obs.OnNext(msg0)
         obs.OnNext(msg1)
 
-        %buf.Should().HaveLength(2)
-        %buf.[0].Should().Be((id, msg0))
-        %buf.[1].Should().Be((id, msg1))
+        buf |> spinWait 1
+
+        %buf.Should().HaveLength(1)
+        %buf[0].Should().Be((id, [ msg0; msg1 ]))
 
     [<Test>]
-    let ``Multiple observers are merged into a single stream`` () =
-        let appSubject = appSubject ()
-        let buf, _ = collect appSubject
-        let idA = Guid.CreateVersion7()
-        let idB = Guid.CreateVersion7()
+    let ``03: Multiple observers are merged into a single stream`` () =
+        let appSubject = appSubject 2
+        let buf, _ = createBufAndSubscribe appSubject
+        let idA = LogSourceId.create ()
+        let idB = LogSourceId.create ()
 
         let oA = appSubject.GetObserver(idA)
         let oB = appSubject.GetObserver(idB)
@@ -70,29 +94,29 @@ module AppSubjectTests =
         oA.OnNext(msgA1)
         oB.OnNext(msgB1)
 
-        %buf.Should().HaveLength(4)
+        buf |> spinWait 2
+
+        %buf.Should().HaveLength(2)
         %buf.Should()
             .SequenceEqual(
                 seq {
-                    (idA, msgA0)
-                    (idB, msgB0)
-                    (idA, msgA1)
-                    (idB, msgB1)
+                    (idA, [ msgA0; msgA1 ])
+                    (idB, [ msgB0; msgB1 ])
                 }
             )
 
     [<Test>]
-    let ``Observers requested at different times are all linked`` () =
-        let appSubject = appSubject ()
-        let buf, _ = collect appSubject
+    let ``04: Observers requested at different times are all linked`` () =
+        let appSubject = appSubject 2
+        let buf, _ = createBufAndSubscribe appSubject
 
-        let idA = Guid.NewGuid()
+        let idA = LogSourceId.create ()
         let oA = appSubject.GetObserver(idA)
         let msgA0 = msg ()
         oA.OnNext(msgA0)
 
         // requested later
-        let idB = Guid.NewGuid()
+        let idB = LogSourceId.create ()
         let oB = appSubject.GetObserver(idB)
         let msgB0 = msg ()
         oB.OnNext(msgB0)
@@ -100,29 +124,34 @@ module AppSubjectTests =
         let msgA1 = msg ()
         oA.OnNext(msgA1)
 
+        let msgB1 = msg ()
+        oB.OnNext(msgB1)
+
+        buf |> spinWaitItems [2; 2]
+
         %buf.Should()
             .SequenceEqual(
                 seq {
-                    (idA, msgA0)
-                    (idB, msgB0)
-                    (idA, msgA1)
+                    (idA, [ msgA0; msgA1 ])
+                    (idB, [ msgB0; msgB1 ])
                 }
             )
 
     [<Test>]
-    let ``OnCompleted on one observer does not complete the merged stream`` () =
-        let appSubject = appSubject ()
-        let buf = ResizeArray<Guid * LogParseMsg>()
+    let ``05: OnCompleted on one observer does not complete the merged stream`` () =
+        let appSubject = appSubject 1
+        let buf = ResizeArray<LogSourceId * LogPosition list>()
         let mutable completed = false
         let _ =
-            (appSubject :> IObservable<Guid * LogParseMsg>).Subscribe(
-                buf.Add,
-                (fun () -> completed <- true))
+            appSubject
+            :> IObservable<LogSourceId * LogPosition seq>
+            |> Observable.subscribeNext
+                (fun (id, positions) -> buf.Add((id, Seq.toList positions)))
 
-        let idA = Guid.NewGuid()
+        let idA = LogSourceId.create ()
         let oA = appSubject.GetObserver(idA)
 
-        let idB = Guid.NewGuid()
+        let idB = LogSourceId.create ()
         let oB = appSubject.GetObserver(idB)
 
         let msgA = msg ()
@@ -136,18 +165,18 @@ module AppSubjectTests =
         %buf.Should()
             .SequenceEqual(
                 seq {
-                    (idA, msgA)
-                    (idB, msgB)
+                    (idA, [ msgA ])
+                    (idB, [ msgB ])
                 }
             )
         %completed.Should().BeFalse()
 
     [<Test>]
-    let ``After OnCompleted further OnNext from same observer is ignored`` () =
-        let appSubject = appSubject ()
-        let buf, _ = collect appSubject
+    let ``06: After OnCompleted further OnNext from same observer is ignored`` () =
+        let appSubject = appSubject 1
+        let buf, _ = createBufAndSubscribe appSubject
 
-        let idA = Guid.NewGuid()
+        let idA = LogSourceId.create ()
         let oA = appSubject.GetObserver(idA)
         let msgA = msg ()
         oA.OnNext(msgA)
@@ -158,71 +187,58 @@ module AppSubjectTests =
         let act = fun () -> oA.OnNext(msg ())
 
         %act.Should().ThrowExactly<ObjectDisposedException, _>()
+        %buf.Should().HaveLength(1)
+        %(fst buf[0]).Should().Be(idA)
+        %(snd buf[0]).Should().SequenceEqual(seq { msgA })
         %buf.Should()
             .SequenceEqual(
-                seq {
-                    (idA, msgA)
-                }
+                [
+                    (idA, [ msgA ] )
+                ]
             )
 
-    [<Test>]
-    let ``ProcessingError messages are propagated as regular OnNext`` () =
-        let appSubject = appSubject ()
-        let buf, _ = collect appSubject
+    //[<Test>]
+    //let ``ProcessingError messages are propagated as regular OnNext`` () =
+    //    let appSubject = appSubject 1
+    //    let buf, _ = createBufAndSubscribe appSubject
 
-        let id = Guid.NewGuid()
-        let o = appSubject.GetObserver(id)
-        let errMsg = errMsg ()
-        o.OnNext(errMsg)
+    //    let id = LogSourceId.create ()
+    //    let o = appSubject.GetObserver(id)
+    //    let errMsg = errMsg ()
+    //    o.OnNext(errMsg)
 
-        %buf.Should()
-            .SequenceEqual(
-                seq {
-                    (id, errMsg)
-                }
-            )
-
-    [<Test>]
-    let ``Late subscriber receives only messages emitted after subscription`` () =
-        let appSubject = appSubject ()
-        
-        let id = Guid.NewGuid()
-        let o = appSubject.GetObserver(id)
-        o.OnNext(msg ())   // emitted before subscription
-
-        let buf, _ = collect appSubject
-        let msg2 = msg ()
-        o.OnNext(msg2)
-
-        %buf.Should()
-            .SequenceEqual(
-                seq {
-                    (id, msg2)
-                }
-            )
+    //    %buf.Should()
+    //        .SequenceEqual(
+    //            seq {
+    //                (id, errMsg)
+    //            }
+    //        )
+    
 
     [<Test>]
-    let ``Many observers stress test`` () =
-        let appSubject = appSubject ()
-        let buf, _ = collect appSubject
+    let ``07: Many observers stress test`` () =
+        let appSubject = appSubject 1
+        let buf, _ = createBufAndSubscribe appSubject
         let msgs =
             [ for _ in 1..50 -> msg () ]
 
         let observers =
-            [ for i in 1..50 -> appSubject.GetObserver(Guid.NewGuid()), i ]
+            [ for i in 1..50 -> appSubject.GetObserver(LogSourceId.create ()), i ]
 
         for (o, i) in observers do
             o.OnNext(msgs |> List.item (i - 1))
 
+        buf |> spinWait 50
+
         %buf.Should().HaveLength(50)
-        %(buf |> Seq.map snd).Should().SequenceEqual(msgs)
+        %(buf |> Seq.map snd |> Seq.concat).Should().SequenceEqual(msgs)
 
     [<Test>]
-    let ``Completed observer is unlinked - new emissions from a fresh observer still flow`` () =
-        let appSubject = appSubject ()
-        let buf, _ = collect appSubject
+    let ``08: Completed observer is unlinked - new emissions from a fresh observer still flow`` () =
+        let appSubject = appSubject 1
+        let buf, _ = createBufAndSubscribe appSubject
 
-        let id = Guid.NewGuid()
+        let id = LogSourceId.create ()
         let oA = appSubject.GetObserver(id)
         let msgA = msg ()
         oA.OnNext(msgA)
@@ -233,38 +249,46 @@ module AppSubjectTests =
         let msgB = msg ()
         oB.OnNext(msgB)
 
+        buf |> spinWaitItems [1; 1]
+
         %buf.Should()
             .SequenceEqual(
                 seq {
-                    (id, msgA)
-                    (id, msgB)
+                    (id, [ msgA ])
+                    (id, [ msgB ])
                 }
             )
 
     [<Test>]
-    let ``Disposing AppSubject completes the merged stream`` () =
-        let appSubject = appSubject ()
+    let ``09: Disposing AppSubject completes the merged stream`` () =
+        let appSubject = appSubject 1
         let mutable completed = false
         let _ =
-            (appSubject :> IObservable<Guid * LogParseMsg>).Subscribe(
-                (fun _ -> ()),
-                (fun () -> completed <- true))
+            appSubject
+            :> IObservable<LogSourceId * LogPosition seq>
+            |> Observable.subscribeCompleted
+                (fun () -> completed <- true)
 
         (appSubject :> IDisposable).Dispose()
         %completed.Should().BeTrue()
 
     [<Test>]
-    let ``Messages are observed in the order emitted across observers`` () =
-        let appSubject = appSubject ()
+    let ``10: Messages are observed in the order emitted across observers`` () =
+        // Если поставить размер буфера 2, то после oA.OnCompleted() нужно подождать,
+        // пока Msg.FinishObserver не обработается агентом, чтобы буфер флушнулся
+        let appSubject = appSubject 1
         let scheduler = TestScheduler.create ()
 
-        let observer = scheduler |> TestScheduler.createObserver
-        let _ = (appSubject :> IObservable<Guid * LogParseMsg>).Subscribe(observer)
+        let testObserver = scheduler |> TestScheduler.createObserver
+        let _ = 
+            appSubject
+            :> IObservable<LogSourceId * LogPosition seq>
+            |> Observable.subscribeObserver(testObserver)
 
-        let idA = Guid.NewGuid()
+        let idA = LogSourceId.create ()
         let oA = appSubject.GetObserver(idA)
 
-        let idB = Guid.NewGuid()
+        let idB = LogSourceId.create ()
         let oB = appSubject.GetObserver(idB)
 
         let msgA0 = msg ()
@@ -281,18 +305,72 @@ module AppSubjectTests =
         scheduler.Start()
 
         let values =
-            observer.Messages
+            testObserver.Messages
             |> Seq.choose (fun m ->
                 match m.Value.Kind with
-                | System.Reactive.NotificationKind.OnNext -> Some m.Value.Value
+                | System.Reactive.NotificationKind.OnNext -> Some (fst m.Value.Value, snd m.Value.Value |> Seq.toList)
                 | _ -> None)
             |> Seq.toList
 
         %values.Should()
-            .SequenceEqual(
-                seq {
-                    (idA, msgA0)
-                    (idB, msgB0)
-                    (idB, msgB1)
+            .HaveSameItemsAs(
+                seq { 
+                    (idA, [ msgA0 ])
+                    (idB, [ msgB0 ])
+                    (idB, [ msgB1 ])
+                }
+            )
+
+    [<Test>]
+    let ``10: Messages are observed in the order emitted across observers with buff count greater than 1`` () =
+        // Если поставить размер буфера 2, то после oA.OnCompleted() нужно подождать,
+        // пока Msg.FinishObserver не обработается агентом, чтобы буфер флушнулся
+        let appSubject = appSubject 50
+        let buf, _ = createBufAndSubscribe appSubject
+
+        let scheduler = TestScheduler.create ()
+
+        let testObserver = scheduler |> TestScheduler.createObserver
+        let _ = 
+            appSubject
+            :> IObservable<LogSourceId * LogPosition seq>
+            |> Observable.subscribeObserver(testObserver)
+
+        let idA = LogSourceId.create ()
+        let oA = appSubject.GetObserver(idA)
+
+        let idB = LogSourceId.create ()
+        let oB = appSubject.GetObserver(idB)
+
+        let msgA0 = msg ()
+        scheduler |> Schedule.actionSpanTicks 10L (fun () -> oA.OnNext(msgA0)) |> ignore
+
+        let msgB0 = msg ()
+        scheduler |> Schedule.actionSpanTicks 20L (fun () -> oB.OnNext(msgB0)) |> ignore
+
+        scheduler |> Schedule.actionSpanTicks 30L (fun () -> oA.OnCompleted()) |> ignore
+
+        let msgB1 = msg ()
+        scheduler |> Schedule.actionSpanTicks 40L (fun () -> oB.OnNext(msgB1)) |> ignore
+
+        scheduler |> Schedule.actionSpanTicks 50L (fun () -> oB.OnCompleted()) |> ignore
+
+        scheduler.Start()
+
+        buf |> spinWaitItems [1; 2]
+
+        let values =
+            testObserver.Messages
+            |> Seq.choose (fun m ->
+                match m.Value.Kind with
+                | System.Reactive.NotificationKind.OnNext -> Some (fst m.Value.Value, snd m.Value.Value |> Seq.toList)
+                | _ -> None)
+            |> Seq.toList
+
+        %values.Should()
+            .HaveSameItemsAs(
+                seq { 
+                    (idA, [ msgA0 ])
+                    (idB, [ msgB0; msgB1 ])
                 }
             )
