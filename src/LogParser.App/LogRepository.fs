4cc7324a27994dcd4cebc0f53a2bd13805a4cb83
p1eXu5 <v1adp1exu5@gmail.com>
2026-07-04 08:29:00 +0300

    wip: LogRepository

--- src/LogParser.App/LogRepository.fs
+++ src/LogParser.App/LogRepository.fs
@@ -1,86 +1,306 @@
 namespace LogParser.App
 
 open System
+open System.Collections.Generic
 open System.IO
 open System.Threading
 open Microsoft.Extensions.Logging
 open LogParser.App.Abstractions
 open System.Threading.Tasks
+open System.Text
+open LogParser
+open LogParser.Types
+open p1eXu5.FSharp.Reactive
 
-
-type private Msg =
-    | ParseFile of ClientId * FilePath * CancellationToken * AsyncReplyChannel<Result<unit, string>>
-
-// --------------------------------------------
-// FileStorage
-// --------------------------------------------
-
-type private State =
+type LogRepository =
     {
-        // not implemented
-        Cts: CancellationTokenSource
+        ParseText: LogSourceId -> LogSourceText -> LogRepositoryParseTextRequestResult
+        Dispose: unit -> unit
     }
-
-module State =
-
-    let init =
+    interface IDisposable with
+        member this.Dispose() =
+            this.Dispose()
+and
+    LogRepositoryConfiguration =
         {
-            Cts = new CancellationTokenSource()
+            ParserSubscriptionBatchSize: int
+        }
+and
+    LogRepositoryParseTextRequestResult =
+        | Accepted
+        | PreviousInProgress
+        | PreviousNotStorred
+and
+    LogRepositoryError =
+        | LogSourceItemInitializationError of LogSourceId
+and
+    LogRepositoryLogger =
+        {
+            LogInitializingLogSourceItem: LogSourceId -> unit
+            LogLogSourceItemInitialized: LogSourceId -> unit
+            LogCreateTmpFileError: LogSourceId -> FileStorageError -> unit
+            LogOpenTmpFileError: LogSourceId -> exn -> unit
+            LogCreateMemoryStreamError: LogSourceId -> exn -> unit
+            LogLogStreamParsedSuccessfully: LogSourceId -> unit
+            LogLogStreamParsingError: LogSourceId -> string -> unit
+        }
+        with
+            static member Console =
+                let now () = DateTimeOffset.Now.ToString("HH':'mm':'ss.fffff")
+                let locker = Lock()
+
+                let log fmt =
+                    Printf.kprintf (fun msg ->
+                        lock locker (fun () ->
+                            printfn "[%s] LogParser.App.AppSubject\n\t%s (Thread #%i)."
+                                (now ())
+                                msg
+                                Thread.CurrentThread.ManagedThreadId)
+                    ) fmt
+                {
+                    LogInitializingLogSourceItem = log "Initializing log source item: %A"
+                    LogLogSourceItemInitialized = log "Log source item has been initialized: %A"
+                    LogCreateTmpFileError = log "Failed to create temp file for %A - %A"
+                    LogOpenTmpFileError = log "Failed to open temp file for %A - %A"
+                    LogCreateMemoryStreamError = log "Failed to create memory stream for %A - %A"
+                    LogLogStreamParsedSuccessfully = log "Log stream %A has been parsed successfully"
+                    LogLogStreamParsingError = log "Log stream %A has not been parsed - %s"
+                }
+
+exception LogParsingException of LogRepositoryError
+
+module LogRepository =
+
+
+    type private LogSource =
+        {
+            Id: LogSourceId
+            Cts: CancellationTokenSource
+            Stream: Stream
+            StreamSource: LogStreamSource
+            ParseTask: Task
+            Logs: LogPosition list
         }
 
-/// <summary>
-/// 
-/// </summary>
-type LogRepository(storageFactory: IStorageFactory<IStorage>, appSubject: IAppSubject, logger: ILogger<LogRepository>) =
+    type private LogSourceState =
+        | Initializing of Task<Result<LogSource, LogRepositoryError>> * CancellationTokenSource
+        | Initialized of LogSource
 
-    let mutable agent : MailboxProcessor<Msg> | null = null
+    type private InnerTaskParams =
+        {
+            Text: LogSourceText
+            FileStorage: FileStorage
+            Cts: CancellationTokenSource
+            LogSourceId: LogSourceId
+            Logger: LogRepositoryLogger
+            Observer: IObserver<LogPosition>
+        }
 
-    let parseText id text reply ct =
-        match storageFactory.Init() with
-        | Error err ->
-            Error err
+    type private State =
+        {
 
-        | Ok storage ->
-            let observer = appSubject.GetObserver id
-            
-            Async.Sta
+            Items:  Map<LogSourceId, LogSourceState>
+        }
 
-    do
-        agent <- new MailboxProcessor<Msg>(
+    type private Msg =
+        | ParseText of LogSourceId * LogSourceText * AsyncReplyChannel<LogRepositoryParseTextRequestResult>
+        | AppendLogBatch of LogSourceId * LogPosition seq
+
+    let private createMemoryStream (logCreateMemoryStreamError: exn -> unit) (text: LogSourceText) (ct: CancellationToken) =
+        try
+            (
+                new CancellableStream(
+                    new MemoryStream(Encoding.UTF8.GetBytes(text.Value)) :> Stream,
+                    ct
+                ) :> Stream
+                , LogStreamSource.MemoryStream
+            )
+            |> Ok
+        with ex ->
+            logCreateMemoryStreamError ex
+            Error ()
+
+    let private initializationLogSourceItemTask (innerTaskParams: InnerTaskParams) =
+        task {
+            let logger = innerTaskParams.Logger
+            let logSourceId = innerTaskParams.LogSourceId
+            do logger.LogInitializingLogSourceItem logSourceId
+
+            let! result = 
+                innerTaskParams.FileStorage.CreateTmpFileTask innerTaskParams.Text innerTaskParams.Cts.Token
+                |> _.ConfigureAwait(false)
+
+            let streamResult =
+                match result with
+                | Ok filePath -> 
+                    try
+                        (
+                            new CancellableStream(
+                                File.Open(filePath |> FilePath.value, FileMode.Open, FileAccess.ReadWrite),
+                                innerTaskParams.Cts.Token
+                            ) :> Stream
+                            , LogStreamSource.TempFile filePath
+                        )
+                        |> Ok
+                    with ex ->
+                        logger.LogOpenTmpFileError logSourceId ex
+                        createMemoryStream (logger.LogCreateMemoryStreamError logSourceId) innerTaskParams.Text innerTaskParams.Cts.Token
+
+                | Error err ->
+                    logger.LogCreateTmpFileError logSourceId err
+                    createMemoryStream (logger.LogCreateMemoryStreamError logSourceId) innerTaskParams.Text innerTaskParams.Cts.Token
+
+            match streamResult with
+            | Ok (stream, streamSource) ->
+                let logSource : LogSource =
+                    {
+                        Id = logSourceId
+                        Cts = innerTaskParams.Cts
+                        Stream = stream
+                        StreamSource = streamSource
+                        ParseTask =
+                            Task.Factory.StartNew(
+                                Action (fun () ->
+                                    let parseParams = innerTaskParams
+                                    let parseResult = TechLogParser.parseStream parseParams.Observer (sprintf "%O" parseParams.LogSourceId) stream
+                                    match parseResult with
+                                    | Ok () -> parseParams.Logger.LogLogStreamParsedSuccessfully parseParams.LogSourceId
+                                    | Error err -> parseParams.Logger.LogLogStreamParsingError parseParams.LogSourceId err
+                                ),
+                                innerTaskParams.Cts.Token,
+                                TaskCreationOptions.PreferFairness ||| TaskCreationOptions.LongRunning,
+                                TaskScheduler.Default
+                            )
+                        Logs = []
+                    }
+
+                do logger.LogLogSourceItemInitialized logSourceId
+
+                return
+                    logSource |> Ok
+            | Error _ ->
+                innerTaskParams.Observer.OnError(LogParsingException (LogRepositoryError.LogSourceItemInitializationError logSourceId))
+                return LogRepositoryError.LogSourceItemInitializationError logSourceId |> Error
+        }
+
+    let private initializingLogSourceItem
+        (fileStorage: FileStorage)
+        (logger: LogRepositoryLogger)
+        (logSourceId: LogSourceId)
+        (text: LogSourceText)
+        (observer: IObserver<LogPosition>)
+        =
+
+        let cts = new CancellationTokenSource()
+
+        let innerTaskParams =
+            {
+                Text = text
+                FileStorage = fileStorage
+                Cts = cts;
+                LogSourceId = logSourceId
+                Logger = logger
+                Observer = observer
+            }
+
+        let task =
+            Task.Factory
+                .StartNew(
+                    Func<objnull, Task<Result<LogSource, LogRepositoryError>>> (fun state -> initializationLogSourceItemTask (state :?> InnerTaskParams)),
+                    innerTaskParams,
+                    cts.Token,
+                    TaskCreationOptions.PreferFairness,
+                    TaskScheduler.Default
+                )
+                .Unwrap()
+                //.ContinueWith()
+
+        LogSourceState.Initializing (task, cts)
+
+    let private handleParseText
+        (appSubject: AppSubject)
+        (initializingLogSourceItem: LogSourceId -> LogSourceText -> IObserver<LogPosition> -> LogSourceState)
+        (logSourceId: LogSourceId)
+        (logSourceText: LogSourceText)
+        (reply: AsyncReplyChannel<LogRepositoryParseTextRequestResult>)
+        (state: State)
+        : State
+        =
+        match state.Items |> Map.tryFind logSourceId with
+        | None ->
+            let observer = appSubject.GetObserver logSourceId
+            let item = initializingLogSourceItem logSourceId logSourceText observer
+
+            reply.Reply(LogRepositoryParseTextRequestResult.Accepted)
+
+            { state with Items = state.Items |> Map.add logSourceId item }
+
+        | Some logSourceItem ->
+            match logSourceItem with
+            | Initializing _ ->
+                reply.Reply(LogRepositoryParseTextRequestResult.PreviousInProgress)
+                state
+
+            | Initialized logSource ->
+                if not (logSource.ParseTask.IsCanceled || logSource.ParseTask.IsFaulted || logSource.ParseTask.IsCompleted) then
+                    reply.Reply(LogRepositoryParseTextRequestResult.PreviousInProgress)
+                    state
+                else
+                    match logSource.StreamSource with
+                    | LogStreamSource.MemoryStream
+                    | LogStreamSource.TempFile _ ->
+                        reply.Reply(LogRepositoryParseTextRequestResult.PreviousNotStorred)
+                        state
+
+                    | LogStreamSource.UserFile _ ->
+                        // TODO: 
+                        logSource.Stream.Dispose()
+                        reply.Reply(LogRepositoryParseTextRequestResult.Accepted)
+                        state
+                        // let newItem = 
+
+
+    let private agent
+        (handleParseText: LogSourceId -> LogSourceText -> AsyncReplyChannel<LogRepositoryParseTextRequestResult> -> State -> State)
+        =
+        new MailboxProcessor<Msg>(
             (fun processor ->
-                let rec running state =
+                let rec running (state: State) =
                     async {
                         let! msg = processor.Receive()
 
                         // not implemented
                         match msg with
-                        | Msg.ParseFile (id, path, ct, reply) ->
-                            reply.Reply ()
-                            return! running state
+                        | Msg.ParseText (logSourceId, logSourceText, reply) ->
+                            return! running (state |> handleParseText logSourceId logSourceText reply)
+
+                        | Msg.AppendLogBatch (logSourceId, logs) ->
+                            let newState =
+                                match state.Items |> Map.tryFind logSourceId with
+                                | Some item -> 
+                                    { state with Items = state.Items |> Map.add logSourceId item }
+                                | None -> state
+                            return! running newState
                     }
 
-                running ()
+                running { Items = Map.empty }
             )
         )
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="id"></param>
-    /// <param name="text">Text to parse.</param>
-    /// <param name="ct">Token for the parsing task creating cancellation.</param>
-    member _.ParseTextAck(id: ClientId, text: string, ct: CancellationToken) =
-        try
-            let tmpFile = Path.GetTempFileName()
-            match FilePath.create tmpFile with
-            | Ok path ->
-                agent.PostAndReply(fun reply -> Msg.ParseFile (id, path, ct, reply))
-                Ok ()
-            | Error err ->
-                logger.LogError(err)
-                Error "Failed to parse text"
-        with ex ->
-            logger.LogError(ex, "Failed to parse text.")
-            Error "Failed to parse text"
+    let init (appSubject: AppSubject) (fileStorage: FileStorage) (logger: LogRepositoryLogger) =
+        let agent = agent (handleParseText appSubject (initializingLogSourceItem fileStorage logger))
 
-    // TODO: other methods
+        let subscription =
+            appSubject
+            :> IObservable<LogSourceId * LogPosition seq>
+            |> Observable.subscribeNext
+                (fun (key, l) ->
+                    agent.Post(Msg.AppendLogBatch(key, l))
+                )
+        {
+            ParseText = fun (logSourceId: LogSourceId) (logSourceText: LogSourceText) ->
+                agent.PostAndReply(fun reply -> Msg.ParseText (logSourceId, logSourceText, reply))
+            Dispose = fun () ->
+                subscription.Dispose()
+                agent.Dispose()
+        }
--- test/LogParser.App.Tests/LogRepositoryTests.fs
+++ test/LogParser.App.Tests/LogRepositoryTests.fs
@@ -1,21 +1,55 @@
 ﻿namespace LogParser.App.Tests
 
 open System
+open System.Threading.Tasks
 open Microsoft.Extensions.Logging
 
 open NUnit.Framework
 open Faqt
 open Faqt.Operators
-open FSharp.Control.Reactive.Testing
-open LogParser.Tests.Fakers
+open p1eXu5.FSharp.Reactive
+open p1eXu5.FSharp.Reactive.Testing
 open p1eXu5.AspNetCore.Testing.Logging
+open LogParser.Tests.Fakers
 
-open FSharp.Control.Reactive
 
 open LogParser.App
 
 module LogRepositoryTests =
 
+    let private appSubject (parserSubscriptionBatchSize: int) =
+        let appConfig =
+            {
+                ParserSubscriptionBatchSize = parserSubscriptionBatchSize
+                ParserBatchFlushTimeSpan = TimeSpan.FromMilliseconds(200.0)
+            }
+        AppSubject.init AppSubjectLogger.Console appConfig
+
+    let private logRepository appSubject =
+        // To force use memory stream
+        let erroredFileStorage : FileStorage =
+            {
+                CreateTmpFileTask = fun _ _ -> Task.FromResult (Error FileStorageError.TmpFileCreatingError)
+            }
+        LogRepository.init appSubject erroredFileStorage LogRepositoryLogger.Console
+
+    let private logText () =
+        let newLine = Environment.NewLine
+        String.Join(
+            newLine,
+            TechLog.generateJsonMany ()
+            |> List.map (fun l -> $"{{{newLine}{l.ToString()}{newLine}}}")
+        )
+        |> LogSourceText.createUnsafe
+
     [<Test>]
-    let ``ParseTextAck`` () =
-        ()
+    let ``ParseText. When text is valid json Then fulfilled item`` () =
+        let appSubject = appSubject 2
+        let logRepository = logRepository appSubject
+        let logText = logText ()
+        let logSourceId = LogSourceId.create ()
+
+        let res = logRepository.ParseText logSourceId logText
+        %res
+            .Should()
+            .BeOfCase(Accepted)
