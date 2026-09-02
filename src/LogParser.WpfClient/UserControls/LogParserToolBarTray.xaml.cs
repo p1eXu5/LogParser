c848ba4a0de0680789e96785ac5e13d2b117200e
p1eXu5 <v1adp1exu5@gmail.com>
2026-09-02 08:52:23 +0300

    chore: add tool bar tray menu items for hiding standard and filters tool bars

--- src/LogParser.WpfClient/UserControls/LogParserToolBarTray.xaml.cs
+++ src/LogParser.WpfClient/UserControls/LogParserToolBarTray.xaml.cs
@@ -36,7 +36,7 @@ public partial class LogParserToolBarTray : UserControl
     public static readonly DependencyProperty RawLogsTogglerProperty =
         DependencyProperty.Register(nameof(RawLogsToggler), typeof(bool), typeof(LogParserToolBarTray), new PropertyMetadata(false));
 
-    private void MenuItem_Click(object sender, RoutedEventArgs e)
+    private void ToggleKibanaToolBarsVisibility(object sender, RoutedEventArgs e)
     {
         if (m_KibanaSearchToolBar.Visibility == Visibility.Visible)
         {
@@ -49,6 +49,42 @@ public partial class LogParserToolBarTray : UserControl
             m_KibanaSearchToolBar.Visibility = Visibility.Visible;
             m_KibanaAccountToolBar.Visibility = Visibility.Visible;
             m_KibanaCheckIcon.Visibility = Visibility.Visible;
+        }
+
+        m_ToolBarTray.InvalidateMeasure();
+
+        e.Handled = true;
+    }
+
+    private void ToggleStandardBarVisibility(object sender, RoutedEventArgs e)
+    {
+        if (m_StandardToolBar.Visibility == Visibility.Visible)
+        {
+            m_StandardToolBar.Visibility = Visibility.Collapsed;
+            m_StandardBarCheckIcon.Visibility = Visibility.Hidden;
+        }
+        else
+        {
+            m_StandardToolBar.Visibility = Visibility.Visible;
+            m_StandardBarCheckIcon.Visibility = Visibility.Visible;
+        }
+
+        m_ToolBarTray.InvalidateMeasure();
+
+        e.Handled = true;
+    }
+
+    private void ToggleFiltersBarVisibility(object sender, RoutedEventArgs e)
+    {
+        if (m_FiltersToolBar.Visibility == Visibility.Visible)
+        {
+            m_FiltersToolBar.Visibility = Visibility.Collapsed;
+            m_FiltersBarCheckIcon.Visibility = Visibility.Hidden;
+        }
+        else
+        {
+            m_FiltersToolBar.Visibility = Visibility.Visible;
+            m_FiltersBarCheckIcon.Visibility = Visibility.Visible;
         }
 
         m_ToolBarTray.InvalidateMeasure();
