48b1ba9679712823b58c7ece4008fe940955b5e7
p1eXu5 <v1adp1exu5@gmail.com>
2026-09-02 09:03:42 +0300

    fmt: cln usings

--- src/LogParser.WpfClient/UserControls/LogParserToolBarTray.xaml.cs
+++ src/LogParser.WpfClient/UserControls/LogParserToolBarTray.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using LogParser.WpfClient.UserControls.ToolBars;
 
 namespace LogParser.WpfClient.UserControls;
 
@@ -26,13 +13,15 @@ public partial class LogParserToolBarTray : UserControl
         InitializeComponent();
     }
 
+    /// <summary>
+    /// For raw logs visibility controlling from menu. <see cref="LogFileContent"/>.
+    /// </summary>
     public bool RawLogsToggler
     {
         get => (bool)GetValue(RawLogsTogglerProperty);
         set => SetValue(RawLogsTogglerProperty, value);
     }
 
-    // Using a DependencyProperty as the backing store for RawLogsToggler.  This enables animation, styling, binding, etc...
     public static readonly DependencyProperty RawLogsTogglerProperty =
         DependencyProperty.Register(nameof(RawLogsToggler), typeof(bool), typeof(LogParserToolBarTray), new PropertyMetadata(false));
 
